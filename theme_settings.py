2bb9010a601bc5eac830e41df88978e9006a24f6
Jacob Sayles <jsayles@gmail.com>
2016-11-17 11:39:30 -0800

    ignore some things

---
+++ theme_settings.py
@@ -0,0 +1,7 @@
+ALLOW_ONLINE_REGISTRATION = False
+ALLOW_PHOTO_UPLOAD = False
+
+FACEBOOK_URL = "https://www.facebook.com/OfficeNomads"
+TWITTER_URL = 'https://twitter.com/OfficeNomads'
+YELP_URL = 'https://www.yelp.com/biz/office-nomads-seattle-2'
+INSTAGRAM_URL = 'https://www.instagram.com/officenomads/'
